6ff242e5b8d348fd53dde63aaa6838065639fbe7
EMNIPUN <nipun20011216@gmail.com>
2025-02-03 01:25:41 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Signup } from './Signup'
-import Login from './Login'
+import { Signup } from '@/pages/Signup'
+import Login from '@/pages/Login'
 
 
 createRoot(document.getElementById('root')).render(
