334c5b34918268fbde0535518409e22dfb71249e
EMNIPUN <nipun20011216@gmail.com>
2025-02-03 20:30:35 +0530

    Comment All NavLink

--- src/components/Navigator/NavigationMenuLink.jsx
+++ src/components/Navigator/NavigationMenuLink.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import {
-    NavigationMenuLink,
-  } from "@/components/ui/navigation-menu"
+// import React from 'react'
+// import {
+//     NavigationMenuLink,
+//   } from "@/components/ui/navigation-menu"
 
 
-function NavigationMenuLinks(props) {
-  return (
-    <NavigationMenuLink className='border-b border-slate-300'>
-    <a
+// function NavigationMenuLinks(props) {
+//   return (
+//     <NavigationMenuLink className='border-b border-slate-300'>
+//     <a
 
-    >
-        <div className="text-lg font-medium">
-         <ul>
-            <li
-                className='hover:bg-slate-100 p-2 rounded-md hover:cursor-pointer'
-            >
-                <p className="text-xm text-black font-normal"><i class={props.name.icon}></i> {props.name.title}</p>
-                <p className="text-xs text-black mt-2 font-light">
-                    {props.name.description}
-                </p>
-            </li>
+//     >
+//         <div className="text-lg font-medium">
+//          <ul>
+//             <li
+//                 className='hover:bg-slate-100 p-2 rounded-md hover:cursor-pointer'
+//             >
+//                 <p className="text-xm text-black font-normal"><i class={props.name.icon}></i> {props.name.title}</p>
+//                 <p className="text-xs text-black mt-2 font-light">
+//                     {props.name.description}
+//                 </p>
+//             </li>
 
-         </ul>
-        </div>
-    </a>
-    </NavigationMenuLink>
-  )
-}
+//          </ul>
+//         </div>
+//     </a>
+//     </NavigationMenuLink>
+//   )
+// }
 
-export default NavigationMenuLinks
+// export default NavigationMenuLinks
