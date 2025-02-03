15255505d603758658ba1669cec1ed4d5962930b
EMNIPUN <nipun20011216@gmail.com>
2025-02-03 20:22:48 +0530

    fifth commit

--- src/components/Navigator/Navigator.jsx
+++ src/components/Navigator/Navigator.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom"
-import { NavLink } from "react-router-dom";
-import NavLink from "./NavLink";
+import NavLink from "@components/Navigator/NavLink";
 
 
 
